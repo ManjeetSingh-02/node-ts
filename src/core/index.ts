f677349dbcaa3b5bdaa4cc0adacbbc6417453db9
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-17 23:21:36 +0530

    add database connection module and export it in index

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,6 +3,9 @@ export { APP_CONFIG } from './config/constants.js';
 export { corsConfig } from './config/cors.js';
 export { env } from './config/env.js';
 
+// database
+export { default as connectWithDatabase } from './database/connect.js';
+
 // loader
 export { default as loadModules } from './loader/modules.js';
 
