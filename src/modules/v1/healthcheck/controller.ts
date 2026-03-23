86656fb1d4e3bbc929e853d10c7409273ba6dead
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-23 11:42:20 +0530

    refactor: update checkHealth method to use ISuccessResponse type and structured response

--- src/modules/v1/healthcheck/controller.ts
+++ src/modules/v1/healthcheck/controller.ts
@@ -2,12 +2,18 @@
 import { SuccessResponse } from '../../../core/index.js';
 
 // type-imports
+import type { ISuccessResponse } from '../../../core/index.js';
 import type { Request, Response } from 'express';
 
 // controller for module
 export const controller = {
   // @controller GET /
-  checkHealth: (_request: Request, response: Response<SuccessResponse>): void => {
-    response.status(200).json(new SuccessResponse('Service is healthy'));
+  checkHealth: (_request: Request, response: Response<ISuccessResponse<null>>) => {
+    return response.status(200).json(
+      new SuccessResponse({
+        message: 'Service is healthy',
+        data: null,
+      })
+    );
   },
 };
