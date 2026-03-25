5946e688b7499ba170be224fa2b999fa75f05fa5
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-26 00:11:23 +0530

    fix: update import statement for createApp to correct path

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 // internal-imports
+import { createApp } from '@/app/index.js';
 import { env, logger } from '@/core/index.js';
-import createApp from '@/app.js';
 
 // external-imports
 import http from 'http';
