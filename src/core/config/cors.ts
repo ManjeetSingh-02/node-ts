92fc73cf357c98a3bc1af7ead85612d2915e89f0
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-07-11 20:40:02 +0530

    default settings removed

--- src/core/config/cors.ts
+++ src/core/config/cors.ts
@@ -8,7 +8,4 @@ import type { CorsOptions } from 'cors';
 export const corsConfig: CorsOptions = {
   origin: env.ORIGIN_URL,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-  exposedHeaders: ['Set-Cookie', '*'],
 } as const;
