dcf004a4fec6c1e3b17c3de250a2a813d3a347d7
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-07-12 00:02:28 +0530

    Enhance asyncHandler to include RequestHandler type and improve type safety

--- src/core/utils/async-handler.ts
+++ src/core/utils/async-handler.ts
@@ -1,18 +1,18 @@
 // type-imports
-import type { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction, RequestHandler } from 'express';
 
 // type definition for the controller that will be wrapped by the async-handler
 type Controller<R extends Request = Request, T = unknown> = (
   request: R,
   response: Response<T>,
-  nextFunction: NextFunction
+  next: NextFunction
 ) => unknown | Promise<unknown>;
 
 // function to wrap controllers inside async-handler
 export function asyncHandler<R extends Request = Request, T = unknown>(
   controller: Controller<R, T>
-) {
-  return function (request: R, response: Response<T>, nextFunction: NextFunction) {
-    return Promise.resolve(controller(request, response, nextFunction)).catch(nextFunction);
+): RequestHandler {
+  return function (request, response, next) {
+    return Promise.resolve(controller(request as R, response as Response<T>, next)).catch(next);
   };
 }
